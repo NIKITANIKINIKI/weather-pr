dd6753c8df0935918d5a927b0858119aa1e3d121
Никита Комаров <nikitakmrv76@gmail.com>
2024-02-21 18:11:29 +0300

    fixed bugs in the old project

--- src/App.js
+++ src/App.js
@@ -1,85 +1,82 @@
-import React from 'react';
-import Form from './components/form';
-import Weather from './components/weather';
+import React from "react";
+import Form from "./components/form";
+import Weather from "./components/weather";
 
-
-const API_KEY='95b1470aaade307a4f22285f12fc65a6';
+const API_KEY = "95b1470aaade307a4f22285f12fc65a6";
 
 class App extends React.Component {
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined,
+  };
 
+  good_time(time) {
+    var date = new Date();
+    date.setTime(time * 1000);
+    return date.getHours() + ":" + date.getMinutes();
+  }
 
-    state={
+  gettingWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+
+    if (city) {
+      try {
+        const api_url = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+        );
+        const data = await api_url.json();
+        console.log(data);
+
+        var sunset = this.good_time(data.sys.sunset);
+        var sunrise = this.good_time(data.sys.sunrise);
+
+        this.setState({
+          temp: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          sunrise: sunrise,
+          sunset: sunset,
+          error: undefined,
+        });
+      } catch (error) {
+        console.log(error);
+        this.setState({
+          error: `Попробуйте еще раз, нам не удалось найти ${city}`,
+        });
+      }
+      e.target.elements.city.value = "";
+    } else {
+      this.setState({
         temp: undefined,
         city: undefined,
         country: undefined,
         sunrise: undefined,
         sunset: undefined,
-        error: undefined
+        error: "Введите название города",
+      });
     }
+  };
 
-
-    good_time(time) {
-
-      var date=new Date();
-      date.setTime(time*1000);
-      return date.getHours()+':'+date.getMinutes();
-    };
-
-    gettingWeather= async (e)=>{
-        e.preventDefault();
-        const city=e.target.elements.city.value;
-        
-        if(city){
-            const api_url= await
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
-            const data = await api_url.json();
-            console.log(data);
-            
-            var sunset=this.good_time(data.sys.sunset);
-            var sunrise=this.good_time(data.sys.sunrise);
-
-            // var date1=new Date();
-            // var date2=new Date();
-            // date1.setTime(sunset); //отслеживаемая дата 
-            // var sunset_date=date1.getHours()+':'+date1.getMinutes()+':'+date1.getSeconds();
-            // date2.setTime(sunrise); //отслеживаемая дата 
-            // var sunrise_date=date2.getHours()+':'+date2.getMinutes()+':'+date2.getSeconds()
-
-            this.setState({
-                temp: data.main.temp,
-                city: data.name,
-                country: data.sys.country,
-                sunrise: sunrise,
-                sunset: sunset,
-                error: undefined            
-            });
-        } else {
-            this.setState({
-                temp: undefined,
-                city: undefined,
-                country: undefined,
-                sunrise: undefined,
-                sunset: undefined,
-                error: 'Введите название города'                 
-            });
-        }
-    }
-
-    render(){
-        return(
-            <div class='form-container' >
-                <Form weatherMethod={this.gettingWeather}/>
-                <Weather
-                    temp={this.state.temp}
-                    city={this.state.city}
-                    country={this.state.country}
-                    sunrise={this.state.sunrise}                    
-                    sunset={this.state.sunset}
-                    error={this.state.error}                
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div class="form-container">
+        <Form weatherMethod={this.gettingWeather} />
+        <Weather
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,21 +1,24 @@
 import React from "react";
 
 class Weather extends React.Component {
-    render() {
-        return(
-            <div>
-            {this.props.city &&
-                <div>
-                    <p>Местоположение: {this.props.city}</p>
-                    <p>Температура: {this.props.temp}</p>
-                    <p>Восход солнца: {this.props.sunrise}</p>
-                    <p>Заход солнца: {this.props.sunset}</p>
-                </div>                
-            }
-            <p class='error'>{this.props.error}</p>
+  render() {
+    return (
+      <div>
+        {this.props.city && (
+          <>
+            <div className="weather-content">
+              <p>Местоположение: {this.props.city}</p>
+              <p>Температура: {Math.round(this.props.temp - 273)} ℃</p>
+              <p>Восход солнца: {this.props.sunrise}</p>
+              <p>Заход солнца: {this.props.sunset}</p>
             </div>
-        );
-    }
+            <img></img>
+          </>
+        )}
+        <p class="error">{this.props.error}</p>
+      </div>
+    );
+  }
 }
 
 export default Weather;
